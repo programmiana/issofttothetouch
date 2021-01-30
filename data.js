5bdbc3d9c2f4b733ab64b5ec301584858b1b7941
Mariana Moreira <m.camargomoreira@gmail.com>
2021-01-30 22:28:16 +0100

    make menu dynamic, make page load better and animateTitle better, clean up code

---
+++ data.js
@@ -0,0 +1,27 @@
+export const menuItems = [
+  {
+    name: "is soft to the touch",
+    href: "./index.html",
+    id: 0
+  },
+  {
+    name: "let your vagina speak",
+    href: "./we-want-your-story.html",
+    id: 1
+  },
+  {
+    name: "about",
+    href: "./about-the-project.html",
+    id: 2 
+  },
+  {
+    name: "who we are",
+    href: "./who-we-are.html",
+    id: 3
+  },
+  {
+    name: "talk to us",
+    href: "./contact-us.html",
+    id: 4
+  },
+];
--- script.js
+++ script.js
@@ -1,92 +1,52 @@
-const menu = document.querySelector(".menu");
+import { menuItems } from "./data.js";
+
 const menuList = document.querySelector(".dropdown-content");
 const menuButton = document.querySelector(".menu-btn");
 const icon = document.querySelector(".icon");
-const iconOnHover = document.querySelector(".icon-on-hover");
+// const iconOnHover = document.querySelector(".icon-on-hover");
 const title = document.getElementsByTagName("span");
-const primaryButton = document.querySelector('.primary-button');
-const backLink = document.querySelector('.back-link');
-const dropdownItems = document.querySelectorAll(".dropdown-item");
-const bigVagina = document.querySelector(".big-vagina");
-const hands = document.querySelector(".hands");
-const contentPage = document.querySelector(".content-page")
+const buttonContainer = document.querySelector(".story");
+const backLink = document.querySelector(".back-link");
+// const bigVagina = document.querySelector(".big-vagina");
+// const hands = document.querySelector(".hands");
 
-Array.from(dropdownItems).forEach((item) =>
-  item.addEventListener("mouseover", handleMenuItemHover)
-);
 menuButton.addEventListener("load", handleMenuClick);
 menuButton.addEventListener("mouseover", handleMenuClick);
 window.addEventListener("click", handleClickOutsideOfDropdown);
-// icon.addEventListener("mouseover", handleIconHover);
-window.addEventListener('load', handleMainPageLoad)
-window.addEventListener('load', handleContentPageLoad)
+window.addEventListener("load", handlePageLoad);
 
+function createMenu(arr) {
+  const ul = document.createElement("ul");
+  const menuContent = createMenuContent(arr);
+  menuList.appendChild(ul);
+  ul.innerHTML = menuContent;
+  const dropdownItems = document.querySelectorAll(".dropdown-item");
 
-// make this a helper function ?
-function handleMainPageLoad(e){
-   if (window.location.href === "http://127.0.0.1:5502/index.html") {
-    const titleArr = Array.from(title)
-    setTimeout(function(){
-        titleArr[0].classList.remove('hidden');
-        titleArr[0].classList.add('fade-in-slower')
-    }, 1000)
-    setTimeout(function(){
-        titleArr[1].classList.remove('hidden');
-        titleArr[1].classList.add('fade-in-slower')
-    }, 1500)
-    setTimeout(function(){
-        titleArr[2].classList.remove('hidden');
-        titleArr[2].classList.add('fade-in-slower')
-    }, 2000)
-    setTimeout(function(){
-        titleArr[3].classList.remove('hidden');
-        titleArr[3].classList.add('fade-in-slower')
-    }, 2500)
-    setTimeout(function(){
-        titleArr[4].classList.remove('hidden');
-        titleArr[4].classList.add('fade-in-slower')
-    }, 3000)
-
-    setTimeout(function(){
-      primaryButton.classList.remove('hidden');
-      primaryButton.classList.add('fade-in');
-      primaryButton.addEventListener('mouseenter', function(e){
-        primaryButton.classList.remove('primary-button');
-        primaryButton.classList.add('primary-button-hover');
-        primaryButton.addEventListener('mouseleave', function(e){
-        primaryButton.classList.remove('primary-button-hover');
-        primaryButton.classList.add('primary-button');
-
-        });
-
-      })
-
-    }, 3500)
-  }
-  else return;
+  menuList.addEventListener("mouseenter", handleMenuItemHover)
 }
 
+function handleMenuItemHover(e) {
+  const dropdownItems = document.querySelectorAll(".dropdown-item");
+  Array.from(dropdownItems).filter((item) => {
+  item.children[0].href === window.location.href &&
+      (item.style.textDecoration = "underline");
+  });
+}
 
+function createMenuContent(arr) {
+  const html = arr
+    .map(
+      (item) =>
+        `<li class="dropdown-item"><a id="${item.id}" href="${item.href}">${item.name}</option></li>`
+    )
+    .join("");
 
-
-function handleContentPageLoad(e){
-  const homeLink = "http://127.0.0.1:5502/index.html"
-  console.log(document.referrer)
-  backLink.setAttribute("href", document.referrer);
-
-  console.log(backLink)
-
-  
+  return html;
 }
 
 function handleMenuClick(e) {
   menuList.classList.toggle("hidden");
-
-  (Array.from(dropdownItems)).filter((item => item.children[0].href === window.location.href ? item.style.textDecoration = 'underline'  : null))
-
 }
-
-
 
 function handleClickOutsideOfDropdown(e) {
   if (
@@ -103,9 +63,55 @@ function handleClickOutsideOfDropdown(e) {
   }
 }
 
-function handleMenuItemHover(e) {
-  (Array.from(dropdownItems)).filter((item => item.children[0].href === window.location.href ? item.style.textDecoration = 'underline'  : null))
+function handlePageLoad(e){
+  createMenu(menuItems);
+  if (window.location.href === "http://127.0.0.1:5502/index.html") {
+    handleMainPageLoad()
+  } else {
+    handleContentPageLoad()
+  }
 }
+
+function handleMainPageLoad() {
+  if (window.location.href === "http://127.0.0.1:5502/index.html") {
+    const titleArr = Array.from(title);
+
+    // make this a promise ?
+    animateTitle(titleArr);
+
+    const primaryButton = document.createElement("a");
+    primaryButton.innerText = "let your vagina speak".toUpperCase();
+    primaryButton.setAttribute("href", "./we-want-your-story.html");
+    primaryButton.classList.add("primary-button");
+    primaryButton.addEventListener("mouseenter", function (e) {
+      primaryButton.classList.remove("primary-button");
+      primaryButton.classList.add("primary-button-hover");
+    });
+
+    buttonContainer.appendChild(primaryButton);
+
+    console.log(buttonContainer);
+  } else return;
+}
+
+function animateTitle(arr) {
+  arr.map((el, i) => {
+    setTimeout(function () {
+      el.classList.remove("hidden");
+      el.classList.add("fade-in-slower");
+    }, i * 1100);
+  });
+}
+
+function handleContentPageLoad() {
+  if (window.location.href !== "http://127.0.0.1:5502/index.html") {
+    const homeLink = "http://127.0.0.1:5502/index.html";
+    console.log(document.referrer);
+    backLink.setAttribute("href", document.referrer);
+    console.log(backLink);
+  }
+}
+
 function handleIconHover(e) {
   e.preventDefault();
 
@@ -114,7 +120,6 @@ function handleIconHover(e) {
 
   icon.addEventListener("mouseleave", function () {
     icon.setAttribute("src", "assets/images/icon.png");
-
   });
 }
 
